49eeaf8ffb601a513ec45584955448d23e96655e
Deepak-kumar2004 <deepak.kumar176362@gmail.com>
2025-02-01 16:56:42 +0530

    Adding NavBar

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ import requests
 app = Flask(__name__)
 
 NEWS_API_KEY = "5f560b41e1a44e22b3f21bde982a0e72"
-NEWS_API_URL = "https://newsapi.org/v2/everything?q=tesla&from=2025-01-01&sortBy=publishedAt&apiKey=5f560b41e1a44e22b3f21bde982a0e72"
-
+NEWS_API_URL = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=5f560b41e1a44e22b3f21bde982a0e72"
 def fetch_news():
     response = requests.get(NEWS_API_URL)
     if response.status_code == 200:
@@ -15,7 +14,6 @@ def fetch_news():
 @app.route("/")
 def home():
     news_articles = fetch_news()
-    print(news_articles)
     return render_template("index.html", news=news_articles)
 
 
